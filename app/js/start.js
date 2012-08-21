81761ac1f6b50fc04df5aacd866ee9e734189dc1
David Mosher <davidmosher@gmail.com>
2012-08-21 17:00:52 -0600

    commentz

--- app/js/start.js
+++ app/js/start.js
@@ -1,11 +1,12 @@
 (function(r, c, m) {
 
+  // Collections
   r.ChallengeActivities = new c.ChallengeActivities();
   r.ChallengeRules = new c.ChallengeRules();
   r.ChallengeBonuses = new c.ChallengeBonuses();
-  r.ChallengeDetail = new m.ChallengeDetail();
 
   // Models
+  r.ChallengeDetail = new m.ChallengeDetail();
   r.ChallengeActivity = new m.ChallengeActivity();
   r.ChallengeRule = new m.ChallengeRule();
   r.ChallengeBonus = new m.ChallengeBonus();
