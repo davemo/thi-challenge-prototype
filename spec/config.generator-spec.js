94bfc61688dc6836403899651a350f65856efa78
David Mosher <davidmosher@gmail.com>
2012-08-23 14:07:46 -0600

    test refactorage

--- spec/config.generator-spec.js
+++ spec/config.generator-spec.js
@@ -3,6 +3,29 @@ describe("THI.Challenge.ConfigGenerator", function() {
   
   subject = THI.Challenge.ConfigGenerator;
   
+  // helpers
+  function addRulesTo(collection, ruleArray) {
+    _.each(ruleArray, function(raw) {
+      collection.add({
+        timePeriod: raw[0],
+        activity: raw[1],
+        constraint: raw[2],
+        points: raw[3]
+      });
+    });
+  }
+  
+  function addBonusesTo(collection, bonusArray) {
+    _.each(bonusArray, function(raw) {
+      collection.add({
+        activity:    raw[0],
+        threshold:   raw[1],
+        timePeriod:  raw[2],
+        bonus:       raw[3]
+      });
+    });
+  }
+  
   beforeEach(function() {
     activities = new THI.Collections.ChallengeActivities;
     rules = new THI.Collections.ChallengeRules();
@@ -48,166 +71,91 @@ describe("THI.Challenge.ConfigGenerator", function() {
     });
   });
   
-  function addRulesTo(collection, ruleArray) {
-    _.each(ruleArray, function(raw) {
-      collection.add({
-        timePeriod: raw[0],
-        activity: raw[1],
-        constraint: raw[2],
-        points: raw[3]
-      });
-    });
-  }
-  
   describe("generating rules", function() {
     
-    context("daily rules", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['day', 'step', 'min', 100], 
-          ['day', 'login', 'max', 200]
-        ]);
-        generatedConfig = subject({ rules: rules });
-      });
+    function testTimePeriodRules(timePeriod, ordinal, rules) {
+      addRulesTo(rules, [
+        [timePeriod, 'step', 'min', 100], 
+        [timePeriod, 'login', 'max', 200]
+      ]);
       
+      expect(subject({ rules: rules }).rules[ordinal]).toEqual({
+        range: timePeriod,
+        rules: [
+          { type: "step", min: 100 }, 
+          { type: "login", max: 200 }
+        ]
+      });
+    }
+    
+    context("daily rules", function() {      
       it("generates the daily rules object as the first member of the rules array", function() {
-        expect(generatedConfig.rules[0]).toEqual({
-          range: "day",
-          rules: [
-            { type: "step", min: 100 }, 
-            { type: "login", max: 200 }
-          ]
-        });
+        testTimePeriodRules('day', 0, rules);
       });
     });
     
-    context("weekly rules", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['week', 'step', 'min', 100], 
-          ['week', 'login', 'max', 200],
-          ['week', 'anything', 'max', 5000]
-        ]);
-        generatedConfig = subject({ rules: rules });
-      });
-      
+    context("weekly rules", function() {      
       it("generates the weekly rules object as the second member of the rules array", function() {
-        expect(generatedConfig.rules[1]).toEqual({
-          range: "week",
-          max: 5000,
-          rules: [
-            { type: "step", min: 100 }, 
-            { type: "login", max: 200 }
-          ]
-        });
+        testTimePeriodRules('week', 1, rules);
       });
     });
     
-    context("monthly rules", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['month', 'step', 'min', 100], 
-          ['month', 'login', 'max', 200],
-          ['month', 'anything', 'min', 5000]
-        ]);
-        generatedConfig = subject({ rules: rules });
-      });
-      
+    context("monthly rules", function() {      
       it("generates the monthly rules object as the third member of the rules array", function() {
-        expect(generatedConfig.rules[2]).toEqual({
-          range: "month",
-          min: 5000,
-          rules: [
-            { type: "step", min: 100 }, 
-            { type: "login", max: 200 }
-          ]
-        });
+        testTimePeriodRules('month', 2, rules);
       });
     });
     
     context("challenge wide rules", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['challenge', 'step', 'min', 100], 
-          ['challenge', 'login', 'max', 200],
-          ['challenge', 'anything', 'min', 5000]
-        ]);
-        generatedConfig = subject({ rules: rules });
-      });
-      
       it("generates the challenge wide rules object as the fourth member of the rules array", function() {
-        expect(generatedConfig.rules[3]).toEqual({
-          range: "challenge",
-          min: 5000,
-          rules: [
-            { type: "step", min: 100 }, 
-            { type: "login", max: 200 }
-          ]
-        });
+        testTimePeriodRules('challenge', 3, rules);
       });
     });
   });
   
-  function addBonusesTo(collection, bonusArray) {
-    _.each(bonusArray, function(raw) {
-      collection.add({
-        activity:    raw[0],
-        threshold:   raw[1],
-        timePeriod:  raw[2],
-        bonus:       raw[3]
-      });
-    });
-  }
-  
   describe("generating bonuses", function() {
     
-    describe("daily bonuses", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['day', 'step', 'min', 100], 
-          ['day', 'login', 'max', 200]
-        ]);
-        addBonusesTo(bonuses, [
-          ['step', 12500, 'day', 1000], 
-          ['login', 200,  'day', 50]
-        ]);
-        generatedConfig = subject({ bonuses: bonuses, rules: rules });
-      });
+    function testTimePeriodBonuses(timePeriod, ordinal, rules, bonuses) {
+      addRulesTo(rules, [
+        [timePeriod, 'step', 'min', 100], 
+        [timePeriod, 'login', 'max', 200]
+      ]);
+      addBonusesTo(bonuses, [
+        ['step', 12500, timePeriod, 1000], 
+        ['login', 200,  timePeriod, 50]
+      ]);
       
+      expect(subject({ bonuses: bonuses, rules: rules }).rules[ordinal]).toEqual({
+        range: timePeriod,
+        rules: [
+          { type: "step", min: 100, bonus: { threshold: 12500, reward: 1000 } }, 
+          { type: "login", max: 200, bonus: { threshold: 200, reward: 50} }
+        ]
+      });
+    }
+    
+    describe("daily bonuses", function() {
       it("includes bonuses in the daily rules calculation", function() {
-        expect(generatedConfig.rules[0]).toEqual({
-          range: "day",
-          rules: [
-            { type: "step", min: 100, bonus: { threshold: 12500, reward: 1000 } }, 
-            { type: "login", max: 200, bonus: { threshold: 200, reward: 50} }
-          ]
-        });
+        testTimePeriodBonuses('day', 0, rules, bonuses);
       });
     });
     
     describe("weekly bonuses", function() {
-      beforeEach(function() {
-        addRulesTo(rules, [
-          ['week', 'step', 'min', 100], 
-          ['week', 'login', 'max', 200]
-        ]);
-        addBonusesTo(bonuses, [
-          ['step', 12500, 'week', 1000], 
-          ['login', 200,  'week', 50]
-        ]);
-        generatedConfig = subject({ bonuses: bonuses, rules: rules });
-      });
-      
       it("includes bonuses in the weekly rules calculation", function() {
-        expect(generatedConfig.rules[1]).toEqual({
-          range: "week",
-          rules: [
-            { type: "step", min: 100, bonus: { threshold: 12500, reward: 1000 } }, 
-            { type: "login", max: 200, bonus: { threshold: 200, reward: 50} }
-          ]
-        });
+        testTimePeriodBonuses('week', 1, rules, bonuses);
       });
     });
-        
+    
+    describe("monthly bonuses", function() {
+      it("includes bonuses in the monthly rules calculation", function() {
+        testTimePeriodBonuses('month', 2, rules, bonuses);
+      });
+    });
+    
+    describe("challenge wide bonuses", function() {
+      it("includes bonuses in the challenge rules calculation", function() {
+        testTimePeriodBonuses('challenge', 3, rules, bonuses);
+      });
+    });
   });
 });
